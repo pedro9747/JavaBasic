0d0a9d37f66d6508fe49c478a93d56e7a16557ba
pedro9747@gmail.com <pedro9747@gmail.com>
2022-08-16 11:35:15 -0300

    Resolución de ejercicio

---
+++ src/javaBasic/Ejercicio.java
@@ -0,0 +1,40 @@
+package javaBasic;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+        // 1. Numéricos
+        // 1.1 Enteros
+
+        byte variable1 = 1;
+        short variable2 = 2;
+        int variable3 = 3;
+        long variable4 = 4;
+
+        //1.2 Decimales
+
+        float variable5 = 5.0f;
+        double variable6 = 6.0d;
+
+        // 2. Booleanos
+
+        boolean variable7 = true;
+        boolean variable8 = false;
+
+        // 3. Alfanuméricos
+
+        char variable9 = 'a';
+        String variable10 = "Variable10";
+
+        System.out.println("Variable byte: " + variable1);
+        System.out.println("Variable short: " + variable2);
+        System.out.println("Variable int: " + variable3);
+        System.out.println("Variable long: " + variable4);
+        System.out.println("Variable float: " + variable5);
+        System.out.println("Variable double: " + variable6);
+        System.out.println("Variable boolean: " + variable7);
+        System.out.println("Variable boolean: " + variable8);
+        System.out.println("Variable char: " + variable9);
+        System.out.println("Variable String: " + variable10);
+
+    }
+}
